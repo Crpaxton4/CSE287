c2a31fcc6ea6683a389ff1769f0a481205fb7bd3
Mike Zmuda <zmudam@miamioh.edu>
2017-09-09 01:20:05 -0400

    Fixed comment.

--- HW1/CSE287Lab/HW1.cpp
+++ HW1/CSE287Lab/HW1.cpp
@@ -78,7 +78,7 @@ float myDotProduct(const glm::vec3 &v1, const glm::vec3 &v2) {
 // Some examples:
 // x1=0, y1=0  x2=10, y2=10     returns 45
 // x1=2, y1=2  x2=2, y2=0       returns 270
-// x1=1, y1=1  x2=3  y2=0       returns 315
+// x1=1, y1=1  x2=2  y2=0       returns 315
 float directionInDegrees(float x1, float y1, float x2, float y2) {
 	return 0;
 }
