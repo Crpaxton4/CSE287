8e3237432c413ed44ec31d52a6b9466ce3b142e6
Mike Zmuda <zmudam@miamioh.edu>
2017-09-18 13:43:51 -0400

    Added HW2 project

---
+++ HW2/HW2/main.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <glm/glm.hpp>
+#include "BasicIncludesAndDefines.h"
+//#include "utitlities.h"
+//#include "parametricLine.h"
+//#include "plane.h"
+//#include "ellipsoid.h"
+//#include "sphere.h"
+//#include "triangle.h"
+
+int main(int argc, char** argv) {
+//	parametricLine pl(origin, glm::vec3(1, 1, 1));
+//	plane plane1(glm::vec3(0, 0, 0), glm::vec3(1, 1, 1));
+//	plane plane2(glm::vec3(0, 0, 1), glm::vec3(1, 0, 1), glm::vec3(1, 1, 1));
+//	ellipsoid ellipsoid1(glm::vec3(0, 0, 0), 2, 3, 4);
+//	sphere sphere1(glm::vec3(-2, 2, 0), 2);
+//	triangle tri1(glm::vec2(0, 0), glm::vec2(1, 0), glm::vec2(1, 1));
+
+	return 0;
+}
