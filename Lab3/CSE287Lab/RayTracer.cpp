5343b669b9052f752cda4bc830fbdf406c6ef328
Mike Zmuda <zmudam@miamioh.edu>
2017-09-21 13:43:24 -0400

    Added Lab3

---
+++ Lab3/CSE287Lab/RayTracer.cpp
@@ -0,0 +1,84 @@
+#include "RayTracer.h"
+
+RayTracer::RayTracer(FrameBuffer & cBuffer, color defaultColor)
+	:colorBuffer(cBuffer), defaultColor(defaultColor), recursionDepth(2) {
+
+}
+
+RayTracer::~RayTracer(void) {
+}
+
+void RayTracer::setCameraFrame(const glm::vec3 & viewPosition, const glm::vec3 & viewingDirection, glm::vec3 up) {
+	// Calculate and set the the w, u, and vdata members and 
+	// set the eye data member to the viewing position.
+	// TODO
+
+
+} // end setCameraFrame
+
+void RayTracer::calculatePerspectiveViewingParameters(const float verticalFieldOfViewDegrees) {
+	// Set the set the topLimit, bottomLimit, rightLimit, 
+	// leftLimit, distToPlane, nx, and ny data members
+	// TODO
+
+
+} // end calculatePerspectiveViewingParameters
+
+void RayTracer::calculateOrthographicViewingParameters(const float viewPlaneHeight) {
+	topLimit = fabs(viewPlaneHeight) / 2.0f;
+
+	rightLimit = topLimit * ((float)colorBuffer.getWindowWidth() / colorBuffer.getWindowHeight()); // Set r based on aspect ratio and height of plane
+
+	// Make view plane symetrical about the viewing direction
+	leftLimit = -rightLimit;
+	bottomLimit = -topLimit;
+
+	// Calculate the distance between pixels in the horizontal and vertical directions
+	nx = (float)colorBuffer.getWindowWidth();
+	ny = (float)colorBuffer.getWindowHeight();
+
+	distToPlane = 0.0; // Rays start on the view plane
+
+} // end calculateOrthographicViewingParameters
+
+glm::vec2 RayTracer::getProjectionPlaneCoordinates(const int x, const int y) {
+	// Page 75
+	// Calculate and return the u and v data members as the x an y coordinates
+	// of the 
+	glm::vec2 uvScalarValues;
+
+	// TODO
+}
+
+void RayTracer::setOrthoRayOriginAndDirection(const int x, const int y) {
+	glm::vec2 uv = getProjectionPlaneCoordinates(x, y);
+
+	// Page 75
+	rayDirection = glm::normalize(-w);
+	rayOrigin = eye + uv.x * u + uv.y * v;
+
+} // end setOrthoOriginAndDirection
+
+void RayTracer::setPerspectiveRayOriginAndDirection(const int x, const int y) {
+
+	// TODO
+
+} // end setPerspectiveOriginAndDirection
+
+void RayTracer::raytraceScene(const std::vector<std::shared_ptr<Surface>> & surfaces, const std::vector<std::shared_ptr<LightSource>> & lights) {
+	this->surfacesInScene = surfaces;
+	this->lightsInScene = lights;
+
+	// Iterate through each and every pixel in the rendering window
+	// TODO
+
+
+} // end raytraceScene
+
+color RayTracer::traceIndividualRay(const glm::vec3 &e, const glm::vec3 &d, int recursionLevel) {
+	// Find surface intersection that is closest to 'e' in the direction 'd.'
+	// TODO
+
+	return defaultColor;
+
+} // end traceRay
